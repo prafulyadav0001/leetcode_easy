daa25d6ae5acfcde12dd8310e14e9900fd37951a
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-05-28 14:45:56 +0530

    Time: 10 ms (23.35%), Space: 39.2 MB (88.00%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1)
+            return n;
+        
+        int f1=fib(n-1);
+        int f2=fib(n-2);
+        int ans=f1+f2;
+        
+        return ans;
+    }
+}
