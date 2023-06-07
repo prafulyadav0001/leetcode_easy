36b74daa7e7d66f256c9c3eedb35f5cdcfcb7cf3
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-06-07 23:14:57 +0530

    Time: 1 ms (95.32%), Space: 43.8 MB (25.92%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -11,21 +11,20 @@ class Solution {
         if (cr == arr.length || cc == arr[0].length) {
 			return Integer.MAX_VALUE;
 		}
-
-		if (cr == arr.length - 1 && cc == arr[0].length - 1) {
-
-			return arr[cr][cc] > 0 ? 1 : 1 - arr[cr][cc];
-
-		}
-		if (dp[cr][cc] != -1) {
-			return dp[cr][cc];
-		}
-		int left = MinimumHP(arr, cr, cc + 1, dp);
-		int right = MinimumHP(arr, cr + 1, cc, dp);
-
-		int hp = Math.min(left, right) - arr[cr][cc];
-
-		return dp[cr][cc] = hp > 0 ? hp : 1;
+       if(cr==arr.length-1 && cc==arr[0].length-1)
+       {
+           return arr[cr][cc]>0?1:1-arr[cr][cc];
+       }
+       
+       if(dp[cr][cc]!=-1){
+           return dp[cr][cc];
+       }
         
-    }
+        int left=MinimumHP(arr,cr,cc+1,dp);
+        int right=MinimumHP(arr,cr+1,cc,dp);
+       
+       int hp= Math.min(left,right)-arr[cr][cc];
+       
+       return dp[cr][cc]= hp>0?hp:1;
+   }
 }
