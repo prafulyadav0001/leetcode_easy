0b064360d5464f994ded64041eacd0f07dd21bcb
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-06-10 22:30:09 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (26.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -9,14 +9,13 @@ class Solution {
         
         
         int dp[] =new int[length];
-        dp[0] =arr[0] ;
-        dp[1] =Math.max(arr[0] , arr[1] ) ;
+       dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
         
         for(int i=2;i<length;i++){
-            dp[i] =Math.max(dp[i-1],dp[i-2] +arr[i]);
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
         }
-        
-        return dp[length-1];     
+        return dp[length -1];
 }
 
 }
