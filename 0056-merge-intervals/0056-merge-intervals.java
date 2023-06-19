ef64c93f72fbb4063026a233423ddb0c6b078c93
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-06-19 23:28:04 +0530

    Time: 10 ms (43.01%), Space: 45.6 MB (84.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
         LinkedList<int[]> merged = new LinkedList<>();
         for (int[] interval : intervals) {
           // agr kuch bc nhi hai 
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0] ){
                 merged.add(interval);
             }
-           // agr kuch chod hai
-            else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            else{
+                merged.getLast()[1] =Math.max(merged.getLast()[1],interval[1]);
             }
         }
         // array mein return hoga
