dbf9501c0038b13e74e7462689626f7b2a6a5ffc
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-06-26 23:41:08 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (99.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,31 +1,26 @@
 class Solution {
-    int[] dp;
+    int dp[];
     public int climbStairs(int n) {
-       dp=new int[n+1];
-        
-        for(int i=0;i<=n;i++)
-        {
+        dp=new int[n+1];
+        for(int i=0;i<dp.length;i++){
             dp[i]=-1;
         }
-        
-      return  recursionbabu(n);
+        return rec(n);
     }
     
-    public int recursionbabu(int n)
-    {
+    public int rec(int n){
         if(dp[n]==-1){
-             int lund;
-        if( n==0 || n==1 || n==2){
-        lund=n;
+              int r=0;
+            if(n==0 || n==1 || n==2 || n==3 ){
+             r=n;
+            }
+            else{
+                r=rec(n-1)+rec(n-2);
+            }
+            dp[n]=r;
+          
         }
-        else{
-            lund=recursionbabu(n-1)+recursionbabu(n-2);
-        }
-        dp[n]=lund;
-        }
-       
-    return dp[n];
+        
+        return dp[n];
     }
-    
-  
 }
