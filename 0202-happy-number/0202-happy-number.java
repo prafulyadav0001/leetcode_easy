ebd4c2b7f7406fbfdce6bff6ef5db2d108241c3f
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-06 12:43:12 +0530

    Time: 1 ms (89.71%), Space: 39.5 MB (85.13%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+      public boolean isHappy(int n) {
+        ArrayList<Integer> l=new ArrayList<Integer>();
+        while (n!=1){
+            int t=n;
+            n=0;
+            while (t>0){
+                n=n+(int) Math.pow((t%10),2);
+                t=t/10;
+            }
+            if(l.contains(n)){
+                return false;
+            }
+            else {
+                l.add(n);
+            }
+        }
+        return true;
+    }
+}
