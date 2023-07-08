a3dbf908432a75ae7ec85a5970864143a1d1435b
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-09 00:09:11 +0530

    Time: 37 ms (97.73%), Space: 55.2 MB (95.45%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -1,19 +1,16 @@
 class Solution {
-    public long putMarbles(int[] weights, int k) {
-        int n = weights.length;
-        
-        int[] pairWeights = new int[n - 1];
-        
-        for (int i = 0; i < n - 1; i++) {
-            pairWeights[i] = weights[i] + weights[i + 1];
+    public long putMarbles(int[] arr, int k) {
+        int n=arr.length;
+        int wt[]=new int[n-1];
+        for(int i=0;i<n-1;i++){
+            wt[i]=arr[i]+arr[i+1];
         }
-        
-        Arrays.sort(pairWeights);
-        long answer = 0;
-        for (int i = 0; i < k - 1; i++) {
-            answer += pairWeights[n - 2 - i] - pairWeights[i];
+        Arrays.sort(wt);
+        long ans=0;
+        for(int i=0;i<k-1;i++){
+            ans+=wt[n-2-i]-wt[i];
         }
 
-        return answer;
+        return ans;
     }
 }
