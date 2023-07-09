72429d3cb4375b0acf67a386c2fb01119515a69e
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-09 21:37:01 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (56.70%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if (root == null) {
+            return false;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean foundX = false;
+            boolean foundY = false;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.val == x) {
+                    foundX = true;
+                }
+
+                if (node.val == y) {
+                    foundY = true;
+                }
+
+                if (node.left != null && node.right != null) {
+                    // Check if x and y are siblings
+                    if ((node.left.val == x && node.right.val == y) || (node.left.val == y && node.right.val == x)) {
+                        return false;
+                    }
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            if (foundX && foundY) {
+                return true;
+            } else if (foundX || foundY) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
