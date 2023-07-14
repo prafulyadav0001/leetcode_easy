3a4a12296e96b3aba6fb94dd9873190986a2bdc7
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-14 23:13:16 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (59.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,19 +8,20 @@ class Solution {
         return rec(n);
     }
     
-    public int rec(int n){
-        if(dp[n]==-1){
-              int r=0;
-            if(n==0 || n==1 || n==2 || n==3 ){
-             r=n;
-            }
-            else{
-                r=rec(n-1)+rec(n-2);
-            }
-            dp[n]=r;
-          
-        }
-        
-        return dp[n];
+      public int rec(int n){
+        if(dp[n]==-1)
+   {
+       int r=0;
+       if(n==0 || n==1 || n==2)
+       {
+           return r=n;
+       }
+       else
+       {
+           dp[n]=rec(n-1)+rec(n-2);
+       }
+         
+   }
+          return dp[n];  
+   }
     }
-}
