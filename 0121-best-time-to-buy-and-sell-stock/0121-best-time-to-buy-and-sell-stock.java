3dfb334610966b056dac8c7b41e81c2269b69193
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-16 23:41:00 +0530

    Time: 2 ms (93.52%), Space: 61.3 MB (27.22%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,7 +3,7 @@ class Solution {
         int min=arr[0];
         int maxprofit=0;
         int i;
-        for(i=0;i<arr.length;i++){
+        for(i=1;i<arr.length;i++){
             min=Math.min(min,arr[i]);
             int profit= arr[i]-min;
             maxprofit=Math.max(maxprofit,profit);
