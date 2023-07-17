a8b2af68e9abd577a67d6c029792472610bc8b20
PRAFUL YADAV <88019337+prafulyadav@users.noreply.github.com>
2023-07-18 00:20:33 +0530

    Time: 2 ms (78.96%), Space: 44.3 MB (64.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int n=nums.length;
+    public boolean canJump(int[] arr) {
+        int n=arr.length;
         boolean dp[] =new boolean [n];
         for (int i=0;i<n;i++){
             dp[i]=false;
@@ -8,14 +8,13 @@ class Solution {
         
         int lmt=0;
         for(int i=0;i<n;i++){
-          
             if(i<=lmt){
-                 dp[i]=true;
-                lmt=Math.max(lmt,nums[i]+i);
-                if(lmt>=n-1){
+            dp[i]=true;
+                lmt=Math.max(lmt,arr[i]+i);
+                if(lmt>=n-1)
                     return true;
-                }
             }
+          
         }
         return false;
     }
